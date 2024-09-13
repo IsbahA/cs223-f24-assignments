6783cb6eb5d93bf3f4070062dd041774661de560
Isbah Ameer <ameer.isbah@gmail.com>
2024-09-12 21:39:20 -0400

    updated exclaim

--- A02/exclaim.c
+++ A02/exclaim.c
@@ -1,5 +1,34 @@
-#include <stdio.h>
+/***************************************************
+ * exclaim.c
+ * Author: Isbah Ameer
+ * Date: September 13th, 2024
+ * Turning words into exclaimations
+ */
 
-int main() {
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+int main(){
+  char userInput[32];
+  char symbol[] = {'@', '#', '!', '*'};
+  int n;
+
+  printf("Enter a word: ");
+  scanf("%s", userInput);
+
+  printf("OMG! "); //print statement starts
+
+  for (int i = 0; i < strlen(userInput); i++){
+    if (userInput[i] >= 'a' && userInput[i] <= 'z'){
+      n = rand() % 4;
+      // replacing lowercase letter with symbol
+      userInput[i] = symbol[n];
+    }
+    printf("%c", userInput[i]);
+
+  }
+  printf("\n");
+
   return 0;
 }
