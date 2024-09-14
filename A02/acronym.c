5ecc4b79a98be5c14ce87817abf10f323fc52e66
Isbah Ameer <ameer.isbah@gmail.com>
2024-09-13 20:34:16 -0400

    style fix

--- A02/acronym.c
+++ A02/acronym.c
@@ -7,26 +7,30 @@
 #include <stdio.h>
 #include <string.h>
 
-int main() {
+int main()
+{
   char userInput[1024];
   char acronym[1024];
-  int index=0;
+  int index = 0;
 
   printf("Enter a phrase: ");
   scanf("%[^\n]%*c", userInput);
 
-//if the first letter of the input is capital, it is added to the acronym
-if (userInput[0]>= 'A'&& userInput[0]<='Z'){
-  acronym[index++] = userInput[0];
-}
-
-for (int i = 1; i < strlen(userInput); i++){
-  if (userInput[i] >= 'A' && userInput[i] <= 'Z' && userInput[i - 1]== ' '){
-    acronym[index++] = userInput[i];
+  // if the first letter of the input is capital, it is added to the acronym
+  if (userInput[0] >= 'A' && userInput[0] <= 'Z')
+  {
+    acronym[index++] = userInput[0];
   }
-}
-acronym[index] = '\0';
 
-printf("Your acronym is %s\n", acronym);
-return 0;
+  for (int i = 1; i < strlen(userInput); i++)
+  {
+    if (userInput[i] >= 'A' && userInput[i] <= 'Z' && userInput[i - 1] == ' ')
+    {
+      acronym[index++] = userInput[i];
+    }
+  }
+  acronym[index] = '\0';
+
+  printf("Your acronym is %s\n", acronym);
+  return 0;
 }
