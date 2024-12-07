4a28761b378a3c7a5e95431672df4a957bd28290
Isbah Ameer <ameer.isbah@gmail.com>
2024-12-07 14:56:10 -0500

    memstats complete

--- A12/memstats.c
+++ A12/memstats.c
@@ -1,10 +1,19 @@
+/*--------------------------------------------------------
+ * Author: Isbah Ameer
+ * Date: December 6th, 2024
+ * Description: Implementation of memstats to print blocks, 
+ * memory allocated and underutilized memory
+ -------------------------------------------------------*/
+#include <stdio.h>
 #include <stdlib.h>
 #include <stdio.h>
 #include <unistd.h>
 #include <sys/time.h>
 #include <time.h>
+#include <math.h>
 #include <string.h>
 #include "rand.h"
+
 
 #define ROUNDS 3
 #define BUFFER 5
@@ -16,8 +25,47 @@ struct chunk {
   struct chunk *next;
 };
 
-void memstats(struct chunk* freelist, void* buffer[], int len) {
+void memstats(struct chunk *freelist, void *buffer[], int len) {
+    // track stats
+    int btotal = 0, bused = 0, bfree = 0;
+    int mtotal = 0, mused = 0, mfree = 0;
+
+    // transverse blocks in buffer
+    for (int i = 0; i < len; i++) {
+        if (buffer[i] != NULL) {
+            struct chunk *current_chunk = (struct chunk *)buffer[i] - 1;
+            btotal++;
+            bused++;
+            mtotal += current_chunk->size;
+            mused += current_chunk->used;
+        }
+    }
+
+    // transverse freelist
+    struct chunk *current = freelist;
+    while (current != NULL) {
+        btotal++;
+        bfree++;
+        mtotal += current->size;
+        mfree += current->size; 
+        current = current->next;
+    }
+
+    //calculate size of heap
+    void *heap_top = sbrk(0);
+    void *heap_start = (void *)freelist;
+    int total_heap_allocated = (char *)heap_top - (char *)heap_start;
+
+    // calculate underutilized memory
+    int underutilized_memory = total_heap_allocated - mused;
+    double underutilized_percent = (double)underutilized_memory / total_heap_allocated;
+
+    // Print statistics
+    printf("Total blocks: %d Free blocks: %d Used blocks: %d\n", btotal, bfree, bused);
+    printf("Total memory allocated: %d Free memory: %d Used memory: %d\n", mtotal, mfree, mused);
+    printf("Underutilized memory: %.2f\n", underutilized_percent);
 }
+
 
 int main ( int argc, char* argv[]) {
 
@@ -35,7 +83,9 @@ int main ( int argc, char* argv[]) {
     buffer[i] = NULL;
   }
 
+
   void *init = sbrk(0);
+
   void *current;
   printf("The initial top of the heap is %p.\n", init);
   for (int j = 0 ; j < ROUNDS; j++) {
@@ -63,7 +113,9 @@ int main ( int argc, char* argv[]) {
       }
     }
     extern struct chunk* flist;
+
     current = sbrk(0);
+
     int allocated = current - init;
     init = current;
 
@@ -78,6 +130,5 @@ int main ( int argc, char* argv[]) {
   gettimeofday(&tend, NULL);
   timer = tend.tv_sec - tstart.tv_sec + (tend.tv_usec - tstart.tv_usec)/1.e6;
   printf("Time is %g\n", timer);
-
   return 0 ;
 }
